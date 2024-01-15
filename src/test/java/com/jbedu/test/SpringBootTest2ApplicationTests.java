2e8dc3226ef163202bb84a2667f22684e68660d5
oopoo123 <oopoo123@naver.com>
2024-01-15 11:29:25 +0900

    스프링 부트 기본 설정

---
+++ src/test/java/com/jbedu/test/SpringBootTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbedu.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
